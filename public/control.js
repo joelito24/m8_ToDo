14e7234ecc37fd2b5785de7b22f0335af6ddceed
Marcos Torrubia Lorca <mtorrubia42@gmail.com>
2023-05-05 17:41:36 +0200

    Feat(Create) - Inicio de funcionalidad

---
+++ public/control.js
@@ -0,0 +1,14 @@
+
+
+var form = document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault(); 
+
+    var event =  $('#event').val();
+    
+    console.log(event);
+    // create()
+
+    
+})
